48c7ded297e08d79c5e88e7850526c3517f23f44
ShoutoSaitou <syouto919@gmail.com>
2021-05-11 04:32:58 +0900

    手順1 商品出品情報を保存するためのモデルとテーブルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_205325) do
+ActiveRecord::Schema.define(version: 2021_05_10_113251) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "delivery_area_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.integer "user", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false

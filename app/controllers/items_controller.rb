4fca21989161289ce3ccc7d7e207f39c69a866a9
ShoutoSaitou <syouto919@gmail.com>
2021-05-15 01:17:34 +0900

    不要なコピペの削除
    ・rubocopが原因かも？

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,31 +32,4 @@ class ItemsController < ApplicationController
       :price
     ).merge(user_id: current_user.id)
   end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def item_params
-    params.require(:item).permit(
-      :image,
-      :name,
-      :description,
-      :category_id,
-      :condition_id,
-      :shipping_charge_id,
-      :delivery_area_id,
-      :days_to_ship_id,
-      :price
-    ).merge(user_id: current_user.id)
-  end
 end

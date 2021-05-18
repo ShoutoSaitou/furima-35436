603d29717d2ed3232cbbc320298521a568cc611b
ShoutoSaitou <syouto919@gmail.com>
2021-05-18 19:48:55 +0900

    売却済みの時のアクセス制限
    ・売却済みの時の、購入ページへのアクセス
    ・売却済みの時の、編集・削除へのアクセス

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
+before_action :set_item,only:[:index,:create]
+before_action :sold_confirmation,only:[:index,:create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
       @order_delivery.save
@@ -21,5 +22,17 @@ class OrdersController < ApplicationController
     params.require(:order_delivery).permit(:postal_code, :delivery_area_id, :municipality, :address, :building, :phone_number).merge(
       user_id: current_user.id, item_id: @item.id
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def sold_confirmation
+    @order= Order.all
+    if @order.find_by(item_id: @item.id)
+      redirect_to root_path
+    end
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,14 +3,15 @@ class OrderDelivery
   attr_accessor :postal_code, :delivery_area_id, :municipality, :address, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :delivery_area_id
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, format: {with: /\A^0\d{9,10}$\z/ }
     validates :user_id
     validates :item_id
   end
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
